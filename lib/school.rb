f2e1749bc7ff9e23c6e72d44d2a237590a104bc5
ali aziz <aa_connect@outlook.com>
2018-07-24 08:15:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,15 +7,6 @@ class School
     @roster = {}
   end
   
-  def add_student(s_name, s_grade)
-  if @roster[s_grade] == nil
-    @roster[s_grade] = []
-    @roster[s_grade]<< s_name
-  else
-    @roster[s_grade]<< s_name
-  end
-
-end
 
 def add_student(student_name, student_grade)
     if @roster[student_grade] == nil
@@ -24,3 +15,4 @@ def add_student(student_name, student_grade)
     else
       @roster[student_grade]<< student_name
     end
+  end
