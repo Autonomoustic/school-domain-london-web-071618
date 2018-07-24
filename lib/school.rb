f8a61e1297eac813fe6f70b6a01f13aa4b69e341
ali aziz <aa_connect@outlook.com>
2018-07-24 08:13:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(s_name, s_grade)
+  if @roster[s_grade] == nil
+    @roster[s_grade] = []
+    @roster[s_grade]<< s_name
+  else
+    @roster[s_grade]<< s_name
+  end
+
+end
